3f8ca4223914ee35c7f5db5cce52d928a7bc760a
39623 <39623@EBI139TFEW.corp.hexaware.com>
2018-07-31 19:15:26 +0530

    Add the CRUD operations

--- SpringBootOracle/src/main/java/com/samik/dao/CustomerRepository.java
+++ SpringBootOracle/src/main/java/com/samik/dao/CustomerRepository.java
@@ -1,10 +1,15 @@
 package com.samik.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.samik.model.Customer;
 
 @CrossOrigin(origins = "http://localhost:4200")
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+	
+	@Query("select c from Customer c where c.id = :id")
+    Customer getCustomerById(@Param("id") Integer id);
 }
--- SpringBootOracle/src/main/java/com/samik/resource/CustomerResource.java
+++ SpringBootOracle/src/main/java/com/samik/resource/CustomerResource.java
@@ -12,18 +12,28 @@ import com.samik.model.Customer;
 @RequestMapping(value = "/rest/customers")
 @CrossOrigin(origins = "http://localhost:4200")
 public class CustomerResource {
-	
+
 	@Autowired
 	CustomerRepository customerRepository;
 
-    @GetMapping(value = "/all")
-    public List<Customer> getAll() {
-        return customerRepository.findAll();
-    }
-    
-    @PostMapping(value = "/load")
-    public void persist(@RequestBody final Customer customer) {
-    	Customer customerSaved = customerRepository.saveAndFlush(customer);
-        System.out.println(customerSaved.getDate());
-    }
+	@GetMapping(value = "/all")
+	public List<Customer> getAll() {
+		return customerRepository.findAll();
+	}
+
+	@GetMapping(value = "/all/{id}")
+	public Customer getCustomerById(@PathVariable("id") Integer id) {
+		return customerRepository.getCustomerById(id);
+	}
+
+	@PostMapping(value = "/load")
+	public void persist(@RequestBody final Customer customer) {
+		Customer customerSaved = customerRepository.saveAndFlush(customer);
+		System.out.println(customerSaved.getDate());
+	}
+
+	@DeleteMapping(value = "/delete/{id}")
+	public void deleteCustomer(@PathVariable("id") Integer id) {
+		customerRepository.delete(id);
+	}
 }
